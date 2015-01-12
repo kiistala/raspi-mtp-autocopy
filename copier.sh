764475a4f378304fce1f5aa6fdb311ca9941e1a9
Ilkka Kiistala <ilkka.kiistala@gmail.com>
2015-01-12 11:57:29 +0200

    added copier.sh, not symlink

--- copier.sh
+++ copier.sh
@@ -1,1 +1,12 @@
-../../../tools/raspbian/copier.sh
+#!/bin/bash
+
+TIMESTAMP=$(date +%Y%m%d-%H%M%S)
+
+SOURCEDIR="/media/SamsungTab3"
+# sudo mkdir -p $SOURCEDIR
+
+DESTDIR="/media/BackupMedia/$TIMESTAMP"
+mkdir -p $DESTDIR
+
+mtpfs -o allow_other $SOURCEDIR && cp -v $SOURCEDIR/*csv $DESTDIR/ && fusermount -u $SOURCEDIR && echo "OK"
+
